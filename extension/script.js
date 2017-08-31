77775614edb29d9c90261bc3fdb632518736d143
Nick Reynolds <me@nickreynolds.net>
2017-08-30 23:05:52 -0700

    Refactor into extracting, start of Giphy support

--- extension/script.js
+++ extension/script.js
@@ -21,9 +21,17 @@ PopcornGif.setup = function(r) {
     Loading : 1,
     Results : 2,
     Empty : 3,
+    Failed : 4,
   }
 
-  var TENOR_API_KEY = 'Y7QV3LZRDJTL';
+  Api = {
+    Tenor : 0,
+    Giphy : 1, // TODO does not work currently
+  }
+
+  var api = Api.Tenor;
+  var API_KEY_TENOR = 'Y7QV3LZRDJTL';
+  var API_KEY_GIPHY = '3rgXBKNKDmcA5Ykg9i'; // 'd3266f0f56f94ea3b4b7eb13f0c7e8f0'; // TODO get our own API key, this is borrow from somewhere temporarily
   var currentTimeout = null;
 
   $('document').ready(function() {
@@ -71,21 +79,27 @@ PopcornGif.setup = function(r) {
 
   var search = function(query) {
     query = query.trim();
-
     clearGifs();
-
     setState(State.Loading);
 
+    switch (api) {
+      case Api.Tenor:
+        url = `https://api.tenor.co/v1/search?tag=${escapeTerm(query)}&limit=50&key=${API_KEY_TENOR}`;
+        parser = parseTenorResponse;
+        break;
+      case Api.Giphy:
+        url = `https://api.giphy.com/v1/gifs/search?q=${escapeTerm(query)}&limit=50&api_key=${API_KEY_GIPHY}`;
+        parser = parseGiphyResponse;
+        break;
+      default:
+        throw "invalid API";
+    }
+
     $.ajax({
-      url: `https://api.tenor.co/v1/search?tag=${escapeTerm(query)}&limit=50&key=${TENOR_API_KEY}`,
-        success: function( result ) {
-          setState(State.Results);
-          setGifs(query, result.results);
-        },
-        failure: function (error) {
-          // TODO show error, maybe new state completely
-          setState(State.Results);
-        },
+      url: url,
+      success: function( result ) { setGifs(query, parser(result)); },
+      failure: function (error) { setState(State.Failed); },
+      beforeSend: function(xhr) { xhr.setRequestHeader("accept", "image/webp,image/apng,image/*,*/*;q=0.8"); },
     });
   };
 
@@ -93,8 +107,29 @@ PopcornGif.setup = function(r) {
     return term.split(' ').map(encodeURIComponent).join('+');
   }
 
+  var parseTenorResponse = function(response) {
+    return response.results.map(function(result) {
+      return createGifObj(result.media[0].tinywebm.url, result.media[0].gif.url, Api.Tenor, result.id);
+    });
+  }
+
+  var parseGiphyResponse = function(response) {
+    return response.data.map(function(result) {
+      return createGifObj(result.images.preview_webp.url, result.embed_url, Api.Giphy, result.id);
+    });
+  }
+
+  var createGifObj = function(webmurl, gifurl, api, id) {
+    return {
+      webmurl : webmurl,
+      gifurl : gifurl,
+      api : api,
+      id : id
+    };
+  }
+
   var setState = function(state) {
-    r.find('#intro').toggle(state == State.Intro);
+    r.find('#intro').toggle(state == State.Intro || state == State.Failed);
     r.find('#loader').toggle(state == State.Loading);
     r.find('#gifs-container').toggle(state == State.Results);
     r.find('#gifs-empty').toggle(state == State.Empty);
@@ -104,20 +139,22 @@ PopcornGif.setup = function(r) {
     r.find('.gif_column').empty();
   }
 
-  var setGifs = function(searchTerm, results) {
-    if (results.length == 0) {
+  var setGifs = function(searchTerm, gifs) {
+    if (gifs.length == 0) {
       setState(State.Empty);
       return;
     }
+
+    setState(State.Results);
 
     var columns = r.find('.gif_column');
 
     // TODO append based on height instead of round robin
 
-    for (var i = 0; i < results.length; i++) {
+    for (var i = 0; i < gifs.length; i++) {
       var container = columns[i % columns.length];
       
-      var element = createGif(searchTerm, results[i]);
+      var element = createGif(searchTerm, gifs[i]);
       container.append(element);
     }
 
@@ -126,17 +163,16 @@ PopcornGif.setup = function(r) {
     r.find('.copy_github_btn').tooltip({delay: 50, position: 'top', tooltip: 'Copy Github markdown'});
   }
 
-  var createGif = function(searchTerm, result) {
-    var media = result.media[0];
-    var tinywebm = media.tinywebm;
-    var gifUrl = media.gif.url;
+  var createGif = function(searchTerm, gif) {
+    var tinywebm = gif.webmurl;
+    var gifUrl = gif.gifurl;
     var githubMarkdown = `![${searchTerm}](${gifUrl})`;
 
     var div = 
       $(`
       <div class="card">
         <div class="card-image">
-          <video autoplay loop muted width="100%"><source src="${tinywebm.url}" type="video/webm"></video>
+          <video autoplay loop muted width="100%"><source src="${tinywebm}" type="video/webm"></video>
         </div>
         <div class="card-content gif-actions">
           <img class="icon-image hand copy_github_btn" src="github.png" />
@@ -148,15 +184,15 @@ PopcornGif.setup = function(r) {
 
     div.find('.save_btn').click(function() {
       downloadFile(gifUrl, searchTerm + ".gif");
-      registerShare(result.id);
+      registerShare(gif);
     });
     div.find('.copy_btn').click(function() {
       copyToClipboard(gifUrl);
-      registerShare(result.id);
+      registerShare(gif);
     });
     div.find('.copy_github_btn').click(function() {
       copyToClipboard(githubMarkdown);
-      registerShare(result.id);
+      registerShare(gif);
     });
 
     // TODO set the gif height accurately so the placeholder doesn't change size
@@ -164,8 +200,12 @@ PopcornGif.setup = function(r) {
     return div[0];
   }
 
-  var registerShare = function(id) {
-    $.ajax({url: `https://api.tenor.co/v1/registershare?id=${id}&key=${TENOR_API_KEY}`}); 
+  var registerShare = function(gif) {
+    switch (gif.api) {
+      case Api.Tenor:
+        $.ajax({url: `https://api.tenor.co/v1/registershare?id=${gif.id}&key=${TENOR_API_KEY}`});
+        break;
+    }
   }
 
   var copyToClipboard = function(text) {
